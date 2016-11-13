2275726440aebcc840f1bf3793d80a750a5fc463
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2016-11-13 17:13:38 +0700

    create main ui

--- app/src/main/java/studio/dinhduc/doctruyen/ui/activity/MainActivity.java
+++ app/src/main/java/studio/dinhduc/doctruyen/ui/activity/MainActivity.java
@@ -1,21 +1,59 @@
 package studio.dinhduc.doctruyen.ui.activity;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import studio.dinhduc.doctruyen.R;
+import studio.dinhduc.doctruyen.ui.constant.Const;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
     ArrayList<String> mNovelNames = new ArrayList<>();
     ArrayAdapter<String> mAdapter;
+    @BindView(R.id.tool_bar) Toolbar mToolBar;
+    @BindView(R.id.lv_main_list_novel) ListView mLvListNovel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
+        initView();
+        getControlWidget();
     }
+
+    private void initView() {
+        setSupportActionBar(mToolBar);
+
+        File f = new File(Const.APP_DIR_PATH);
+        if (!(f.mkdirs() || f.isDirectory())) {
+            return;
+        }
+        ArrayList<File> novelList = new ArrayList<>(Arrays.asList(f.listFiles()));
+        for (int i = 0; i < novelList.size(); i++) {
+            mNovelNames.add(novelList.get(i).getName());
+        }
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mNovelNames);
+        mLvListNovel.setAdapter(mAdapter);
+    }
+
+    private void getControlWidget() {
+        mLvListNovel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String novelDirPath = Const.APP_DIR_PATH + File.separator + mNovelNames.get(position);
+            }
+        });
+    }
+
 }
--- app/src/main/java/studio/dinhduc/doctruyen/ui/constant/Const.java
+++ app/src/main/java/studio/dinhduc/doctruyen/ui/constant/Const.java
@@ -1,9 +1,17 @@
 package studio.dinhduc.doctruyen.ui.constant;
 
+import android.os.Environment;
+
+import java.io.File;
+
 /**
  * Created by dinhduc on 13/11/2016.
  */
 
 public class Const {
-    
+    public static final String APP_DIR_NAME = "DocTruyen";
+    public static final String EXTERNAL_STORAGE_PATH =
+            Environment.getExternalStorageDirectory().getAbsolutePath();
+    public static final String APP_DIR_PATH = EXTERNAL_STORAGE_PATH + File.separator + APP_DIR_NAME;
+
 }
